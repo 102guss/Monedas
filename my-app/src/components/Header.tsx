b62d13b666d7a5d15b54ee2923185f58d1afa53a
102guss <brown_321@live.com>
2026-03-25 17:19:38 -0600

    Ceacion de header

---
+++ my-app/src/components/Header.tsx
@@ -0,0 +1,15 @@
+const Header = () => (
+  <header>
+    <img src="/src/assets/vite.svg" alt="logo" />
+    <nav>
+      <ul>
+        <li>Inicio</li>
+        <li>Acerca de</li>
+        <li>Conjunto</li>
+      </ul>
+    </nav>
+    <button>Iniciar sesión</button>
+  </header>
+)
+
+export default Header
