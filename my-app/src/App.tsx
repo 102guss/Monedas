ace39ce8439060cf6a3dfd42a0b1a1a9e1a13ce2
102guss <brown_321@live.com>
2026-03-25 20:24:28 -0600

    se agregan componntes coin, con propiedades al componente app

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,27 +1,19 @@
-import Header from "./components/Header";
-import Course from "./components/Course";
+
+import Coin from "./components/Coin";
 
 function App() {
   return (
-    <>
-      <Course
-        title="Curso de React"
-        description="Aprende React dede cero"
-      />
-      <Course
-        title="Curso de TypeScript"
-        description="Aprende TypeScript desde cero"
-      />
-
-      <Course
-        title="Curso de Angular"
-        description="Aprende Angular desde cero"
-      />
-      <Course
-        title="Curso de Vue.js"
-        description="Aprende Vue.js desde cero"
-      />
-    </>
+   <div>
+     <Coin order={1} name="Bitcoin" symbol="BTC" price={100000} priceChange={10} code="BTC" />
+     <Coin order={2} name="Ethereum" symbol="ETH" price={100000} priceChange={10} code="ETH" />
+     <Coin order={3} name="Litecoin" symbol="LTC" price={100000} priceChange={10} code="LTC" />
+     <Coin order={4} name="Bitcoin Cash" symbol="BCH" price={100000} priceChange={10} code="BCH" />
+     <Coin order={5} name="Ripple" symbol="XRP" price={100000} priceChange={10} code="XRP" />
+     <Coin order={6} name="Bitcoin Cash XRP" symbol="XRP" price={100000} priceChange={10} code="XRP" />
+     <Coin order={7} name="Bitcoin" symbol="BTC" price={100000} priceChange={10} code="BTC" />
+     <Coin order={8} name="Bitcoin Cash ABC" symbol="BCHABC" price={100000} priceChange={10} code="BCHABC" />
+     <Coin order={9} name="Bitcoin Cash ABC" symbol="BCHABC" price={100000} priceChange={10} code="BCHABC" />
+   </div>
   );
 }
 
