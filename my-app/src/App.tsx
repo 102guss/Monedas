d6aa3971a612a4e4c6479663c5960cb16f071dea
102guss <brown_321@live.com>
2026-03-25 21:14:21 -0600

    recorido de areglo con map y popiedades

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,20 +1,20 @@
+import Coin from './components/Coin'
+import coins from './data/coins'
 
-import Coin from "./components/Coin";
+const App = () => (
+  <div>
+    {coins.map(coin => (
+      <Coin 
+        key={coin.order}
+        order={coin.order}
+        name={coin.name}
+        symbol={coin.symbol}
+        price={coin.price}
+        priceChange={coin.priceChange}
+        code={coin.code}
+      />
+    ))}
+  </div>
+)
 
-function App() {
-  return (
-   <div>
-     <Coin order={1} name="Bitcoin" symbol="BTC" price={100000} priceChange={10} code="BTC" />
-     <Coin order={2} name="Ethereum" symbol="ETH" price={100000} priceChange={10} code="ETH" />
-     <Coin order={3} name="Litecoin" symbol="LTC" price={100000} priceChange={10} code="LTC" />
-     <Coin order={4} name="Bitcoin Cash" symbol="BCH" price={100000} priceChange={10} code="BCH" />
-     <Coin order={5} name="Ripple" symbol="XRP" price={100000} priceChange={10} code="XRP" />
-     <Coin order={6} name="Bitcoin Cash XRP" symbol="XRP" price={100000} priceChange={10} code="XRP" />
-     <Coin order={7} name="Bitcoin" symbol="BTC" price={100000} priceChange={10} code="BTC" />
-     <Coin order={8} name="Bitcoin Cash ABC" symbol="BCHABC" price={100000} priceChange={10} code="BCHABC" />
-     <Coin order={9} name="Bitcoin Cash ABC" symbol="BCHABC" price={100000} priceChange={10} code="BCHABC" />
-   </div>
-  );
-}
-
-export default App;
+export default App
