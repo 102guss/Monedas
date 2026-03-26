d957c8f968db400ca162f2a6ca63b92c83ce858f
102guss <brown_321@live.com>
2026-03-26 16:15:05 -0600

    Se agregò funciòn handleSearch para comparar texto en el input de la búsqueda

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,20 +1,35 @@
 import Coin from './components/Coin'
 import coins from './data/coins'
+import { useRef, useState } from 'react'
 
-const App = () => (
-  <div>
-    {coins.map(coin => (
-      <Coin 
-        key={coin.order}
-        order={coin.order}
-        name={coin.name}
-        symbol={coin.symbol}
-        price={coin.price}
-        priceChange={coin.priceChange}
-        code={coin.code}
-      />
-    ))}
-  </div>
-)
+const App = () => {
+  const [coinsList, setCoinsList] = useState(coins)
+  const searchInput = useRef(null)
 
+const handleSearch = () => {
+  const searchValue = searchInput.current?.value || ''
+  const newCoinsList = coins.filter(coin => coin.name.toLowerCase().includes(searchValue.toLowerCase()))
+  setCoinsList(newCoinsList)
+}
+
+  return (
+    <main>
+      <h1>Lista de criptomonedas</h1>
+      <input type="text" placeholder="Buscar criptomoneda" ref={searchInput} onChange={handleSearch}/>
+      <div>
+        {coinsList.map(coin => (
+          <Coin 
+            key={coin.order}
+            order={coin.order}
+            name={coin.name}
+            symbol={coin.symbol}
+            price={coin.price}
+            priceChange={coin.priceChange}
+            code={coin.code}
+          />
+        ))}
+      </div>
+    </main>
+  )
+}
 export default App
