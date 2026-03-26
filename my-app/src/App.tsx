28ab0402c0b541473282e9d5886fb6f685f7b63e
102guss <brown_321@live.com>
2026-03-26 17:39:06 -0600

    Se agregò un fondo blanco a lista de monedas

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,22 +1,39 @@
-import Coin from './components/Coin'
-import coins from './data/coins'
-import { useRef, useState } from 'react'
-import type { CoinInterface } from './interfaces/Coin'
-import Header from './components/Header'
+import Coin from "./components/Coin";
+import coins from "./data/coins";
+import { useRef, useState } from "react";
+import type { CoinInterface } from "./interfaces/Coin";
+import Header from "./components/Header";
 
 const App = () => {
-  const [coinsList, setCoinsList] = useState<CoinInterface[]>(coins)
-  const searchInput = useRef<HTMLInputElement>(null)
+  const [coinsList, setCoinsList] = useState<CoinInterface[]>(coins);
+  const searchInput = useRef<HTMLInputElement>(null);
 
-const handleSearch = () => {
-  const searchValue = searchInput.current?.value || ''
-  const newCoinsList = coins.filter(coin => coin.name.toLowerCase().includes(searchValue.toLowerCase()))
-  setCoinsList(newCoinsList)
-}
+  const handleSearch = () => {
+    const searchValue = searchInput.current?.value || "";
+    const newCoinsList = coins.filter((coin) =>
+      coin.name.toLowerCase().includes(searchValue.toLowerCase()),
+    );
+    setCoinsList(newCoinsList);
+  };
 
   return (
-    <Header />
-   
-  )
-}
-export default App
+    <div>
+      <Header />
+      <main className="bg-white">
+        <h1>Lista de criptomonedas</h1>
+        <input
+          type="text"
+          placeholder="Buscar criptomoneda"
+          ref={searchInput}
+          onChange={handleSearch}
+        />
+        <div className="coins-list">
+          {coinsList.map((coin) => (
+            <Coin key={coin.order} {...coin} />
+          ))}
+        </div>
+      </main>
+    </div>
+  );
+};
+export default App;
