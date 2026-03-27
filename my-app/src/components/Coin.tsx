2d9fbfe742f7bdbf9a7e49088fdb221959725391
102guss <brown_321@live.com>
2026-03-26 18:18:43 -0600

    Se agregaron estilos tailwind a la tabla de monedas

--- my-app/src/components/Coin.tsx
+++ my-app/src/components/Coin.tsx
@@ -1,29 +1,39 @@
-import { useState } from "react"
-import type { CoinInterface } from "../interfaces/Coin"
+import { useState } from "react";
+import type { CoinInterface } from "../interfaces/Coin";
 
-const Coin = ({order, name, symbol, price, priceChange, code}: CoinInterface) => {
+const Coin = ({
+  order,
+  name,
+  symbol,
+  price,
+  priceChange,
+  code,
+}: CoinInterface) => {
+  const [isFavorite, setIsFavorite] = useState<boolean>(false);
 
-const [isFavorite, setIsFavorite] = useState<boolean>(false)
-
-const handleFavorites = () => {
-  setIsFavorite(!isFavorite)
-}
+  const handleFavorites = () => {
+    setIsFavorite(!isFavorite);
+  };
   return (
-  <tr className="coin-card">
-    <td>{order}</td>
-    <td>{name}</td>
-    <td>{code}</td>
-    <td>{symbol}</td>
-    <td>{price}</td>
-    <td>{priceChange}</td>
-    <td>
-      <button onClick={handleFavorites}>
-        {
-          isFavorite ? "Quitar de favoritos" : "Agregar a favoritos"
-        }
-      </button>
-    </td>
-  </tr>
-)
-}
-export default Coin
+    <tr className="coin-card">
+      <td>{order}</td>
+      <td>
+        <div className="flex items-center gap-3">
+          <img src={symbol} alt={code} className="w-10 h-10 rounded-full" />
+          <div>
+            <span className="font-semibold">{name}</span>
+            <span className="text-sm text-gray-500">{code}</span>
+          </div>
+        </div>
+      </td>
+      <td>{price}</td>
+      <td>{priceChange}</td>
+      <td>
+        <button onClick={handleFavorites}>
+          {isFavorite ? "Quitar de favoritos" : "Agregar a favoritos"}
+        </button>
+      </td>
+    </tr>
+  );
+};
+export default Coin;
