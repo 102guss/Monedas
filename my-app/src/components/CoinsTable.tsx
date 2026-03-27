df0be51026731af93759473b4bf153744a8294fb
102guss <brown_321@live.com>
2026-03-26 18:21:42 -0600

    Se crea componente conisTable

---
+++ my-app/src/components/CoinsTable.tsx
@@ -0,0 +1,22 @@
+const CoinsTable = () => {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Orden</th>
+          <th>Nombre</th>
+          <th>Código</th>
+          <th>Simbolo</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>1</td>
+          <td>Bitcoin</td>
+          <td>BTC</td>
+          <td>BTC</td>
+        </tr>
+      </tbody>
+    </table>
+  );
+};
