e99f1dec4390eb990c7ed198d9404ef472396776
102guss <brown_321@live.com>
2026-04-10 23:24:10 -0600

    Me conectè a una API por medio de una URL para extraer informacion, asì como tambièn agregè un hook de useEfeact para controlar el estado de renderizado

--- my-app/src/components/CoinsContainer.tsx
+++ my-app/src/components/CoinsContainer.tsx
@@ -1,16 +1,28 @@
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import type { CoinInterface } from "../interfaces/Coin";
-import coins from "../data/coins";
 import CoinsTable from "./CoinsTable";
 import CoinsNotFound from "./CoinsNotFound";
 
 const CoinsContainer = () => {
-  const [coinsList, setCoinsList] = useState<CoinInterface[]>(coins);
+  const [coinsList, setCoinsList] = useState<CoinInterface[]>([]);
+  const [coinsListOriginal, setCoinsListOriginal] = useState<CoinInterface[]>([]);
   const searchInput = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+  fetch("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&x_cg_demo_api_key=CG-JWUb7ZnoaHYTgkqqjBviqXBV")
+    .then(response => response.json())
+    .then(data => {
+      setCoinsList(data)
+      setCoinsListOriginal(data)
+    })
+    .catch(error => {
+      console.error("Error al obtener los datos:", error)
+    })
+}, [])
 
   const handleSearch = () => {
     const searchValue = searchInput.current?.value || "";
-    const newCoinsList = coins.filter((coin) =>
+    const newCoinsList = coinsListOriginal.filter((coin) =>
       coin.name.toLowerCase().includes(searchValue.toLowerCase()),
     );
     setCoinsList(newCoinsList);
