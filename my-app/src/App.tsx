3f1ce87b5df0643fa5e201b91a0bd93c7e68d260
102guss <brown_321@live.com>
2026-05-01 14:12:48 -0600

    Se agragó la librería BrouserRouter para crear una SPA, se creò la primer ruta raiz

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,19 +1,18 @@
-
-import Header from "./components/Header";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Layout from "./components/Layout";
 import CoinsContainer from "./components/CoinsContainer";
+
 
 const App = () => {
  
   return (
-    <>
-      <Header />
-      <main className="container mx-auto  py-8">
-     
-        <div className="coins-list">
-          <CoinsContainer />
-        </div>
-      </main>
-    </>
+    <BrowserRouter>
+ 
+      <Routes>
+        <Route path="/" element={<Layout />} />
+        <Route index element={<CoinsContainer />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 export default App;
