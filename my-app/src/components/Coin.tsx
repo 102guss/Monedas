998403a4b23a7b532e0268c2f88f481da4e5f7a4
102guss <brown_321@live.com>
2026-05-03 23:03:31 -0600

    Se agrega una ruta link a cada moneda

--- my-app/src/components/Coin.tsx
+++ my-app/src/components/Coin.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import type { CoinInterface } from "../interfaces/Coin";
+import { Link } from "react-router-dom";
 
 const Coin = ({
 
@@ -20,14 +21,13 @@ const Coin = ({
     <tr className="coin-card [&>td]:px-4 [&>td]:py-3">
       <td className="px-6 py-4 text-sm text-gray-600">{id}</td>
       <td className="px-6 py-4">
-        <div className="flex items-center gap-3">
-          <img src={image} alt={symbol} className="w-10 h-10 rounded-full" />
-          <div className="flex flex-col">
-  <span className="font-semibold text-gray-900">{name}</span>
-  <span className="text-sm text-gray-500">{symbol}</span>
-
-          </div>
+       <Link to={`/coin/${id}`} className="flex items-center gap-3">
+        <img src={image} alt={symbol} className="w-10 h-10 rounded-full" />
+        <div className="flex flex-col">
+          <span className="font-semibold text-gray-900">{name}</span>
+          <span className="text-sm text-gray-500">{symbol}</span>
         </div>
+       </Link>
       </td>
       <td>{current_price}</td>
       <td>{price_change_percentage_24h?.toFixed(2)}%</td>
