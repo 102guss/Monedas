af46d02e5f6912051be4590ecd335bcf0b8319ca
102guss <brown_321@live.com>
2026-05-05 15:36:45 -0600

    Se hizo la peticion a la primer moneda bitcoin pero mediante las bariables d eentorno de gueko, pero para todas las mnedas, solo seà bitcoin

--- my-app/src/components/coinContainer.tsx
+++ my-app/src/components/coinContainer.tsx
@@ -1,7 +1,23 @@
+import { useEffect, useState } from "react"
+import { COINGECKO_API_KEY, URL_API, URL_COINS } from "../constants/api"
+
 const CoinContainer = () => {
+    const [Coin, setCoin] = useState(null)
+
+useEffect(() => {
+  fetch(`${URL_API}${URL_COINS}&x_cg_demo_api_key=${COINGECKO_API_KEY}&ids=bitcoin`)
+  .then(response => response.json())
+  .then(data => {
+    setCoin(data[0])
+  })
+  .catch(error => {
+    console.error("Error al obtener los datos:", error)
+  })
+}, [])
     return (
         <div>
             <h1>Coin Container</h1>
+            <p>{JSON.stringify(Coin)}</p>
         </div>
     )
 }
