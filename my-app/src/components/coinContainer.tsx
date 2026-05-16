cba66cc8d201b2485e514279b3bb3462447a0ecc
102guss <brown_321@live.com>
2026-05-16 16:39:30 -0600

    Se agregó un estado para setLoading y setError para que el componente cambiè de estado antes de cargar la vista en lugar de epezar en nulo

--- my-app/src/components/coinContainer.tsx
+++ my-app/src/components/coinContainer.tsx
@@ -4,17 +4,33 @@ import { useParams } from "react-router-dom"
 
 const CoinContainer = () => {
     const [Coin, setCoin] = useState(null)
+    const [loading, setLoading] = useState(true)
+    const [error, setError] = useState(null)
     const { id } = useParams()
+
+
 useEffect(() => {
   fetch(`${URL_API}${URL_COINS}&x_cg_demo_api_key=${COINGECKO_API_KEY}&ids=${id}`)
   .then(response => response.json())
   .then(data => {
     setCoin(data[0])
+    setLoading(false)
   })
   .catch(error => {
     console.error("Error al obtener los datos:", error)
+    setError(error)
+    setLoading(false)
+    return <div>Error: {error}</div>
   })
-}, [])
+}, [id])
+
+    if (loading) {
+        return <div>Loading...</div>
+    }
+
+    if (error) {
+        return <div>Error: {error}</div>
+    }
     return (
         <div>
             <h1>Coin Container</h1>
